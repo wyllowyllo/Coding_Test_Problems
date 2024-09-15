f5a90f8bb349cc047651ca13a07377e537ea8c39
wyllowyllo <105272901+wyllowyllo@users.noreply.github.com>
2024-09-15 18:10:38 +0900

    baekjoon_4375
    (a*b)%c == ((a%c) * (b%c))%c

---
+++ problems/Algorithm_Codes/Algorithm_Codes/Scripts/4375.cpp
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+	int n;
+	//---------------------My Solution-----------------------------------------------
+	
+	//int ret = 1;
+	//while (scanf("%d", &n)!=EOF) { //테스트케이스가 정확히 명시되지 않았으므로, 입력이 올때마다 받는다.
+	//	long long multiple = 1;
+	//	while (multiple % n != 0) {
+	//		(multiple *= 10) += 1;
+	//		multiple %= n;
+	//		ret++;
+	//	}
+
+	//	printf("%lld\n", ret);
+	//	ret = 1;
+	//}
+	//------------------------------------------------------------------------------
+
+	//---------------------Solution 1-----------------------------------------------
+	while (scanf("%d", &n) != EOF) {
+		int cnt = 1, ret = 1;
+		if (cnt % n == 0) {
+			printf("%d\n", ret);
+			break;
+		}
+		else {
+			cnt = (cnt * 10) + 1;
+			cnt %= n;
+			ret++;
+		}
+	}
+	return 0;
+}
